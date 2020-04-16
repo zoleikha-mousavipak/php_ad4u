b14f46a41ac995475b663699f54052730352f0c5
zoleikha <zmpak2000@gmail.com>
2020-04-16 18:10:05 +0200

    seed

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'admin',
+                'password' => Hash::make('admin@admin.com'),
+                'rememberToken' => 'admin@admin.com',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => 'admin@admin.com',
+                'admin' => true,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'zoleikha',
+                'password' => Hash::make('zmpak2000@gmail.com'),
+                'rememberToken' => 'zmpak2000@gmail.com',
+                'email' => 'zmpak2000@gmail.com',
+                'email_verified_at' => 'zmpak2000@gmail.com',
+                'admin' => true,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+
+        ];
+        DB::table('users')->insert($users);
+    }
+}
